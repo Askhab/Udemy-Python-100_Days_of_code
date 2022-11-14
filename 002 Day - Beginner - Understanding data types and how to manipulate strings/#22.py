0143ea41842b769f923922cb51be3cfe13065860
Askhab <asmag86@gmail.com>
2022-11-14 22:27:45 +0300

    in the middle of the Day 2

---
+++ 002 Day - Beginner - Understanding data types and how to manipulate strings/#22.py
@@ -0,0 +1,15 @@
+# 3 + 5
+# 7 - 3
+# 3 * 2
+# 6 / 3
+# 2 ** 3
+
+# PEMDAS
+# P - Parentheses      - ()
+# E - Exponents        - **
+# M - Multiplication   - *
+# D - Division         - /
+# A - Addition         - +
+# S - Subtraction      - -
+
+print(3 * (3 + 3) / 3 - 3)
