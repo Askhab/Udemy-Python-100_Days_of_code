f2e2ea09e39664c01e859167b9dab56852048ace
Askhab <asmag86@gmail.com>
2023-01-15 16:52:08 +0300

    End 017 day

---
+++ 017 Day - Intermediate - The Quiz Project & the Benefits of OOP/data.py
@@ -0,0 +1,74 @@
+question_data = [
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "Han Solo's co-pilot and best friend, 'Chewbacca', is an Ewok.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "The movie 'The Nightmare before Christmas' was all done with physical objects.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "The film '2001: A Space Odyssey' was released on December 31st, 2000.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "Matt Damon played an astronaut stranded on an extraterrestrial planet in both of the movies Interstellar and The Martian.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "The word 'Inception' came from the 2010 blockbuster hit 'Inception'.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "Shaquille O'Neal appeared in the 1997 film 'Space Jam'.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "In Alfred Hitchcock's film 'Psycho' it is said he used chocolate syrup to simulate the blood in the famous shower scene from ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "George Lucas directed the entire original Star Wars trilogy.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "question": "'Foodfight!' earned less than $80,000 at box office.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
