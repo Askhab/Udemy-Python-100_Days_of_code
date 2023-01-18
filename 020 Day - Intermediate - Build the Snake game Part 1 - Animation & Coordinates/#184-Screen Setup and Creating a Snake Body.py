1ab5f46b9dfbe51aa4d1facfaf6f344a57fbdc50
Askhab <asmag86@gmail.com>
2023-01-18 23:53:44 +0300

    End day 020

---
+++ 020 Day - Intermediate - Build the Snake game Part 1 - Animation & Coordinates/#184-Screen Setup and Creating a Snake Body.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+
+segments = []
+
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+screen.exitonclick()
